d0cf80bd6bd1b10ba4891e21a1505510ea6f9fb1
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-04-15 22:17:09 -0700

    Add files via upload

---
+++ Day1SingleNumber/Day1SingleNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day1SingleNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num;
+            Console.WriteLine(SingleNumber(num = new int[] { 1, 2, 1, 2, 4 }));
+        }
+        public static int SingleNumber(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while( right >= 0)
+            {
+                if( right == left)
+                {
+                    right--;
+                }
+                else if(nums[left] == nums[right])
+                {
+                    left++;
+                    right = nums.Length - 1;
+                }
+                else if(nums[left] != nums[right])
+                {
+                    right--;
+                }
+            }
+            return nums[left];
+        }
+    }
+}
