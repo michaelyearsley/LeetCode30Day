15925315a06752d11d3bd2b8dbd42f5154ecf501
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-04-17 22:40:40 -0700

    Add files via upload

--- Day2HappyNumber/Day2HappyNumber/Program.cs
+++ Day2HappyNumber/Day2HappyNumber/Program.cs
@@ -6,7 +6,34 @@ namespace Day2HappyNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsHappy(7));
+        }
+        public static bool IsHappy(int n)
+        {
+            bool happyNum = true;
+            int num = n;
+            int count = 0;
+            while (num != 1)
+            {
+                if (count > 50)
+                {
+                    return false;
+                }
+                else
+                {
+                    string strNum = Convert.ToString(num);
+                    num = 0;
+                    int num2;
+                    for (int i = 0; i < strNum.Length; i++)
+                    {
+                        num2 = int.Parse(Convert.ToString(strNum[i]));
+                        double power = Math.Pow(num2, 2);
+                        num = Convert.ToInt32(power) + num;
+                    }
+                    count++;
+                }
+            }
+            return happyNum;
         }
     }
 }
